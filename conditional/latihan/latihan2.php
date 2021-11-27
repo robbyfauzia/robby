ab3de372d4620a0ea5a55eb6907913d9f83b357e
robbyfauzia <robbyfauzia123@gmail.com>
2021-11-27 16:17:25 +0700

    menambahkan file conditional

--- conditional/latihan/latihan2.php
+++ conditional/latihan/latihan2.php
@@ -1,10 +1,10 @@
 <?php
 
-$ipk = "2.2";
+$ipk = "2.3";
 
 if ($ipk > 0 && $ipk <= 2) {
     echo "Grade E. maaf anda harus mengulang";
-}elseif ($ipk > 2.1 && $ipk <= 2.3) {
+}elseif ($ipk > 2.1 && $ipk <= 2.6) {
     echo "Grade D. maaf anda harus memperbaiki mata kuliah anda";
 }elseif ($ipk > 2.6 && $ipk <= 2.9) {
     echo "Grade C. mau memperbaiki boleh tidak juga boleh";
